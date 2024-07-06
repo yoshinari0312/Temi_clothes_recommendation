b34adc10bc5f1e3981e39bfc9dddb36a447924d0
Kanta-Tachikawa <kanta1009@keio.jp>
2024-07-06 16:01:37 +0900

    add KT_test_temi

---
+++ test/KT_test_temi/app/src/main/java/com/example/kt_test_temi/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.kt_test_temi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import com.robotemi.sdk.Robot;
+import com.robotemi.sdk.listeners.OnRobotReadyListener;
+
+public class MainActivity extends AppCompatActivity implements OnRobotReadyListener {
+    private static Robot mRobot;
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // Initialize robot instance
+        mRobot = Robot.getInstance();
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        // Add robot event listeners
+        mRobot.addOnRobotReadyListener(this);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        // Remove robot event listeners
+        mRobot.removeOnRobotReadyListener(this);
+    }
+
+    @Override
+    public void onRobotReady(boolean isReady) {
+        if (isReady) {
+            Log.i(TAG, "Robot is ready");
+            mRobot.hideTopBar(); // hide temi's ActionBar when skill is active
+        }
+    }
+}
